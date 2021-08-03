7b78bfe67f26e2fd77f2675e1bcd781b48778c97
Doc Ritezel <pair+doc@ministryofvelocity.com>
2021-08-03 15:28:43 -0700

    Create an example Buyer when seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Buyer.create_with(
+  full_name: 'District Buyer',
+  password: 'password',
+  password_confirmation: 'password'
+).find_or_create_by!(email: 'user@example.com')
