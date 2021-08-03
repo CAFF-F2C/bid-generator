fe9a4cd4fed02eb4b70f04ce4b402af02c6e84d1
Doc Ritezel <pair+doc@ministryofvelocity.com>
2021-08-03 15:27:53 -0700

    Configure Rails to print development logs to the console

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,12 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
