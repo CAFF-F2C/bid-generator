163c307b64f7b77595c559457d343de794ca9cc2
Erica Love and Jeff Parr <pair+el+jp@ministryofvelocity.com>
2021-08-31 13:29:10 -0700

    Add link to item list template to env variables

--- config/initializers/bid_generator.rb
+++ config/initializers/bid_generator.rb
@@ -1,1 +1,2 @@
 Rails.application.config.external_resources_link = ENV.fetch('EXTERNAL_RESOURCES_LINK', '')
+Rails.application.config.item_list_template_link = ENV.fetch('ITEM_LIST_TEMPLATE_LINK', '')
