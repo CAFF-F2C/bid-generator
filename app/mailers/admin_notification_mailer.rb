8790bb16fd7a69a088c32577456ac198dd39773f
Erica Love and Jeff Parr <pair+el+jp@ministryofvelocity.com>
2021-09-23 11:45:56 -0700

    Don’t send notifications if no admin is signed up

--- app/mailers/admin_notification_mailer.rb
+++ app/mailers/admin_notification_mailer.rb
@@ -2,6 +2,7 @@ class AdminNotificationMailer < ApplicationMailer
   def new_registration(buyer)
     @buyer = buyer
     mail_to = AdminUser.where(notify_signup: true).pluck(:email)
+    return if mail_to.blank?
     mail(to: mail_to, subject: 'New Buyer Registration')
   end
 end
