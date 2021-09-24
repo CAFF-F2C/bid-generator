92e75e1f3538ce562b056adab26de9538357e7ba
Erica Love and Jeff Parr <pair+el+jp@ministryofvelocity.com>
2021-09-24 13:28:42 -0700

    Buyer can post their final RFP

--- config/initializers/bid_generator.rb
+++ config/initializers/bid_generator.rb
@@ -1,3 +1,4 @@
 Rails.application.config.external_resources_link = ENV.fetch('EXTERNAL_RESOURCES_LINK', '')
 Rails.application.config.item_list_template_link = ENV.fetch('ITEM_LIST_TEMPLATE_LINK', '')
+Rails.application.config.post_rfp_link = ENV.fetch('POST_RFP_LINK', '')
 Rails.application.config.google_maps_api_key = ENV.fetch('GOOGLE_MAPS_API_KEY', '')
