a7a64183aef7c57239a00398ee012dc74416d1bc
Doc Ritezel <pair+doc@ministryofvelocity.com>
2021-11-10 10:19:16 -0800

    Buyer sees a district profile warning that does not cover the login notice

--- app/controllers/buyers/documents_controller.rb
+++ app/controllers/buyers/documents_controller.rb
@@ -2,6 +2,7 @@ class Buyers::DocumentsController < ApplicationController
   layout 'buyers'
 
   def index
+    flash.now[:warning] = t('.complete_your_profile_html', complete_link: view_context.link_to(t('.complete_link_html'), edit_buyers_district_profile_path, class: 'flash__link')).html_safe unless current_buyer.district_profile&.complete?
     @documents = current_buyer.rfps.order(updated_at: :desc).page(params[:page] || 1).per(params[:per_page] || Kaminari.config.default_per_page)
   end
 end
