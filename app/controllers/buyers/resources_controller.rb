f2d1f440733a7bf7dfecfcef85ad40d053cdfae1
Jeff Parr <jeff@ministryofvelocity.com>
2025-02-12 09:12:49 -0800

    deprications

--- app/controllers/buyers/resources_controller.rb
+++ app/controllers/buyers/resources_controller.rb
@@ -2,6 +2,6 @@ class Buyers::ResourcesController < Buyers::ApplicationController
   skip_before_action :authenticate_buyer!
 
   def show
-    redirect_to Rails.application.config.external_resources_link
+    redirect_to Rails.application.config.external_resources_link, allow_other_host: true
   end
 end
