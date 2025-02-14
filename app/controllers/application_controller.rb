7d887a25baa0f336fd5e217e2bf4182f8f848fd8
Jeff Parr <jeff@ministryofvelocity.com>
2025-02-14 00:37:51 -0800

    fix default form builder
    fix default form builder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  default_form_builder ::ComponentFormBuilder
 
   def after_sign_in_path_for(resource)
     return admin_root_path if resource.instance_of?(AdminUser)
--- config/initializers/form_builder.rb
+++
@@ -1,4 +0,0 @@
-require 'forms/form_builder'
-Rails.application.config.to_prepare do
-  ActionView::Base.default_form_builder = FormBuilder
-end
